4ae2cdeb1e2a04a4107d4f22da714063fa88b7d5
MORPHEUS\Fynn <fynn@fynnyoung.com>
2023-03-09 11:46:06 +0200

    add toggling colour of grid squares on click

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const grid = document.querySelector(".grid-container");
 
 for (let i = 0; i < 16; i++)    {
     const div = document.createElement("div");
-    div.textContent = i;
     div.classList.add("grid-item");
-    div.style.cssText = "border: 1px solid;";
+    div.style.cssText = "background-color: white; border: 1px solid;";
+    div.addEventListener("click", () => {
+        if (div.style.backgroundColor == "white")
+            div.style.backgroundColor = "black";
+        else
+            div.style.backgroundColor = "white";
+    });
     grid.appendChild(div);
 }
