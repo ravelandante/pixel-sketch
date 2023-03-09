0721a3ec5f2e8103ff92386e67ffe53796af6b44
MORPHEUS\Fynn <fynn@fynnyoung.com>
2023-03-09 15:06:19 +0200

    add resizing of canvas

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-const GRID_DIMS = 49;
-const GRID_SIZE = 400;
-const GRID_ITEMS = Math.sqrt(GRID_DIMS);
 
-const grid = document.createElement("div");
-grid.class = "grid-container";
-grid.style.cssText = `display: grid; grid-template-columns: repeat(${GRID_ITEMS}, ${GRID_SIZE/GRID_ITEMS}px); grid-template-rows: repeat(${GRID_ITEMS}, ${GRID_SIZE/GRID_ITEMS}px);`
-document.querySelector("body").appendChild(grid);
 
-for (let i = 0; i < GRID_DIMS; i++)    {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    div.style.cssText = "background-color: white; border: 1px solid;";
-    div.addEventListener("mousedown", () => {
-        if (div.style.backgroundColor == "white")
-            div.style.backgroundColor = "black";
-        else
-            div.style.backgroundColor = "white";
-    });
-    grid.appendChild(div);
+function createGrid(grid_dims, grid_size)   {
+    const old_grid = document.querySelector(".grid-container");
+    if (old_grid)   {
+        old_grid.remove();
+    }
+
+    const grid = document.createElement("div");
+
+    let grid_items = Math.sqrt(grid_size);
+    let grid_item_size = grid_dims/grid_items;
+    grid.classList.add("grid-container");
+    grid.style.cssText = `display: grid; grid-template-columns: repeat(${grid_items}, ${grid_item_size}px); grid-template-rows: repeat(${grid_items}, ${grid_item_size}px); margin: auto;`
+
+    document.querySelector(".flex").appendChild(grid);
+
+    for (let i = 0; i < grid_size; i++)    {
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.style.cssText = `background-color: white; border: ${grid_item_size/100}px solid;`;
+        div.addEventListener("mouseover", () => {
+            if (div.style.backgroundColor == "white")
+                div.style.backgroundColor = "black";
+            else
+                div.style.backgroundColor = "white";
+        });
+        grid.appendChild(div);
+    }
 }
+
+function resizeCanvas() {
+    const size = document.querySelector("#size").value;
+    let grid_min = Math.min(window.innerHeight, window.innerWidth);
+    createGrid(grid_min, size);
+}
+
+resizeCanvas();
+
+const resize = document.querySelector("#resize");
+
+resize.addEventListener("click", () => {
+    resizeCanvas();
+});
