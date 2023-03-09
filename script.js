0b698606dbd4d18b0d51227ad50fa97035456e85
MORPHEUS\Fynn <fynn@fynnyoung.com>
2023-03-09 11:59:37 +0200

    add auto resizing of grid

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const grid = document.querySelector(".grid-container");
+const GRID_DIMS = 49;
+const GRID_SIZE = 400;
+const GRID_ITEMS = Math.sqrt(GRID_DIMS);
 
-for (let i = 0; i < 16; i++)    {
+const grid = document.createElement("div");
+grid.class = "grid-container";
+grid.style.cssText = `display: grid; grid-template-columns: repeat(${GRID_ITEMS}, ${GRID_SIZE/GRID_ITEMS}px); grid-template-rows: repeat(${GRID_ITEMS}, ${GRID_SIZE/GRID_ITEMS}px);`
+document.querySelector("body").appendChild(grid);
+
+for (let i = 0; i < GRID_DIMS; i++)    {
     const div = document.createElement("div");
     div.classList.add("grid-item");
     div.style.cssText = "background-color: white; border: 1px solid;";
-    div.addEventListener("click", () => {
+    div.addEventListener("mousedown", () => {
         if (div.style.backgroundColor == "white")
             div.style.backgroundColor = "black";
         else
